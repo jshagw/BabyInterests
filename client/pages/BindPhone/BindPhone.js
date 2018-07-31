5123c4b12332e321c006c4221e525f23015d7d62
gaowei <13857120590@qq.com>
2018-07-31 16:39:37 +0800

    生成用户二维码

--- client/pages/BindPhone/BindPhone.js
+++ client/pages/BindPhone/BindPhone.js
@@ -1,6 +1,8 @@
 var qcloud = require('../../vendor/wafer2-client-sdk/index')
 var config = require('../../config')
 var util = require('../../utils/util.js')
+var session = require('../../vendor/wafer2-client-sdk/lib/session.js')
+var QR = require('../../utils/qrcode.js')
 var interval = null
 
 Page({
@@ -13,7 +15,10 @@ Page({
     disabled: false,
     verifyText: "获取验证码",
     currentTime: 61,
-    inputPhone:""
+    inputPhone:"",
+    canvasHidden: false,
+    maskHidden: true,
+    imagePath: ''
   },
 
   submit: function(event) {
@@ -132,6 +137,56 @@ Page({
     })
   },
 
+  //适配不同屏幕大小的canvas
+  setCanvasSize: function () {
+    var size = {};
+    try {
+      var res = wx.getSystemInfoSync();
+      var scale = 750 / 500;//不同屏幕下canvas的适配比例；设计稿是750宽
+      var width = res.windowWidth / scale;
+      var height = width;//canvas画布为正方形
+      size.w = width;
+      size.h = height;
+    } catch (e) {
+      // Do something when catch error
+      console.log("获取设备信息失败" + e);
+    }
+    return size;
+  },
+
+  createQrCode: function (url, canvasId, cavW, cavH) {
+    //调用插件中的draw方法，绘制二维码图片
+    QR.api.draw(url, canvasId, cavW, cavH);
+    setTimeout(() => { this.canvasToTempImage(); }, 1000);
+
+  },
+  //获取临时缓存照片路径，存入data中
+  canvasToTempImage: function () {
+    var that = this;
+    wx.canvasToTempFilePath({
+      canvasId: 'mycanvas',
+      success: function (res) {
+        var tempFilePath = res.tempFilePath;
+        console.log(tempFilePath);
+        that.setData({
+          imagePath: tempFilePath,
+          canvasHidden:true
+        });
+      },
+      fail: function (res) {
+        console.log(res);
+      }
+    });
+  },
+  //点击图片进行预览，长按保存分享图片
+  previewImg: function (e) {
+    var img = this.data.imagePath;
+    wx.previewImage({
+      current: img, // 当前显示图片的http链接
+      urls: [img] // 需要预览的图片http链接列表
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -143,7 +198,10 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+    var size = this.setCanvasSize();//动态设置画布大小
+    //调用插件中的draw方法，绘制二维码图片
+    var initUrl = session.get()
+    this.createQrCode(initUrl, "mycanvas", size.w, size.h);
   },
 
   /**
